30442e4e48d2a9ed2b5466455b9ce278db023791
Polina Poznyak <user@MacBook-Pro---PPP.local>
2024-12-21 16:55:13 +0100

    refactor: update Ship CoreData model

--- ShipTechTask/ShipTechTask/CoreData/ShipEntity+CoreDataClass.swift
+++ ShipTechTask/ShipTechTask/CoreData/ShipEntity+CoreDataClass.swift
@@ -1,15 +1,29 @@
-//
-//  ShipEntity+CoreDataClass.swift
-//  ShipTechTask
-//
-//  Created by Polina Poznyak on 20/12/2024.
-//
-//
+import Foundation
+import CoreData
 
-//import Foundation
-//import CoreData
-//
-//@objc(ShipEntity)
-//public class ShipEntity: NSManagedObject {
-//
-//}
+@objc(ShipEntity)
+public class ShipEntity: NSManagedObject {}
+
+extension ShipEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ShipEntity> {
+        NSFetchRequest<ShipEntity>(entityName: "ShipEntity")
+    }
+
+    @NSManaged public var id: String
+    @NSManaged public var name: String
+    @NSManaged public var image: String?
+    @NSManaged public var type: String
+    @NSManaged public var dateBuild: Date?
+
+    convenience init(context: NSManagedObjectContext, with ship: Ship) {
+        self.init(context: context)
+        id = ship.id
+        name = ship.name
+        image = ship.image
+        type = ship.type
+        dateBuild = ship.dateBuild
+    }
+}
+
+extension ShipEntity : Identifiable {}
--- ShipTechTask/ShipTechTask/Ship.swift
+++ ShipTechTask/ShipTechTask/Ship.swift
@@ -1,23 +1,25 @@
+import Foundation
+
 struct Ship: Decodable {
     let id: String
     let name: String
     let image: String?
     let type: String
-    let year: Int?
+    let dateBuild: Date?
 
     enum CodingKeys: String, CodingKey {
         case id
         case name
         case image
         case type
-        case year = "year_built"
+        case dateBuild = "year_built"
     }
 
     init(entity: ShipEntity) {
-        self.id = entity.id ?? ""
-        self.name = entity.name ?? ""
+        self.id = entity.id
+        self.name = entity.name
         self.image = entity.image
-        self.type = entity.type ?? ""
-        self.year = entity.year == 0 ? nil : Int(entity.year)
+        self.type = entity.type
+        self.dateBuild = entity.dateBuild
     }
 }
