cd13ed2aaa61fa5cf5ef33f44a3ef4b2448a10b0
Polina Poznyak <user@MacBook-Pro---PPP.local>
2024-12-22 18:29:42 +0100

    refactor: update ShipsListViewModel

--- ShipTechTask/ShipTechTask/Moduls/ShipsListScreen/ViewModel/ShipsListViewModel.swift
+++ ShipTechTask/ShipTechTask/Moduls/ShipsListScreen/ViewModel/ShipsListViewModel.swift
@@ -20,35 +20,23 @@ final class ShipListViewModel {
 
     // MARK: - Fetch Ships
 
-    func fetchShips() {
-        Task {
-            do {
-                let fetchedShips = try await dataProvider.fetchShips(refreshFromAPI: true)
-                DispatchQueue.main.async {
-                    self.ships = fetchedShips.sorted { $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending }
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.errorMessage = "Failed to fetch ships."
-                }
-            }
+    func fetchShips() async {
+        do {
+            let fetchedShips = try await dataProvider.fetchShips(refreshFromAPI: true)
+            ships = fetchedShips.sorted { $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending }
+        } catch {
+            errorMessage = "Failed to fetch ships."
         }
     }
 
     // MARK: - Delete Ship
 
-    func deleteShip(by id: String) {
-        Task {
-            do {
-                try await dataProvider.deleteShip(by: id)
-                DispatchQueue.main.async {
-                    self.ships.removeAll { $0.id == id }
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.errorMessage = "Failed to delete ship."
-                }
-            }
+    func deleteShip(by id: String) async {
+        do {
+            try await dataProvider.deleteShip(by: id)
+            ships.removeAll { $0.id == id }
+        } catch {
+            errorMessage = "Failed to delete ship."
         }
     }
 }
