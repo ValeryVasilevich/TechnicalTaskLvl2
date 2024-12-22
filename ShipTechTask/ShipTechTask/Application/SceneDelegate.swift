ad5ace1f985f32a85919ee81063e638cc3169e34
Polina Poznyak <user@MacBook-Pro---PPP.local>
2024-12-22 20:36:32 +0100

    refactor: replace AppDelegate and SceneDelegate

--- ShipTechTask/ShipTechTask/Application/SceneDelegate.swift
+++ ShipTechTask/ShipTechTask/Application/SceneDelegate.swift
@@ -1,15 +1,23 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+    private var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
         self.window = window
+
+        let diContainer = DependencyFactory(window: window)
+
+        let appCoordinator = diContainer.makeAppCoordinator(
+            authenticationProvider: diContainer.makeAuthenticationProvider(),
+            dataProvider: diContainer.makeDataProvider()
+        )
+
+        self.appCoordinator = appCoordinator
+        appCoordinator.start()
     }
 }
