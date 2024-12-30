fea14b18396f31bc870a58e9656c906fd5e56203
Polina P <polina.pozniak@innowise.com>
2024-12-30 13:08:30 +0100

    refactor: rename shipsRequest property to shipsRequestFactory

--- ShipTechTask/ShipTechTask/Networking/NetworkServices/ShipsNetworkService.swift
+++ ShipTechTask/ShipTechTask/Networking/NetworkServices/ShipsNetworkService.swift
@@ -1,13 +1,13 @@
 struct ShipsNetworkService {
-    private let shipsRequest = ShipsRequestFactory()
+    private let shipsRequestFactory = ShipsRequestFactory()
 
     func fetchShips() async throws -> [Ship] {
-        let request = try shipsRequest.getShips()
+        let request = try shipsRequestFactory.getShips()
         return try await NetworkManager.shared.performRequest(request, decodeTo: [Ship].self)
     }
 
     func fetchShip(by id: String) async throws -> Ship {
-        let request = try shipsRequest.getShip(by: id)
+        let request = try shipsRequestFactory.getShip(by: id)
         return try await NetworkManager.shared.performRequest(request, decodeTo: Ship.self)
     }
 }
